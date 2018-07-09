9d27468418de7c691557b08ab881688e30d3c715
Ray-mech <7secworks@gmail.com>
2018-07-09 23:31:11 +0900

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,24 @@
+import threading
+import time
+
+"""タイマークラス"""
+class TimerClass(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    self.event = threading.Event()
+    self.count = 24
+
+  def run(self):
+    while self.count > 0 and not self.event.is_set():
+      print (self.count)
+      self.count -= 1
+      self.event.wait(1)
+
+  def stop(self):
+    self.event.set()
+
+"""メイン"""
+tmr = TimerClass()
+tmr.start()
+
+#tmr.stop()でストップ
